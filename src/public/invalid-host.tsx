56b837039795c06e4cc5461fd54b3016dd355b44
Aminul Islam <me@aminul.net>
2023-01-12 21:59:46 +0600

    change invalid host

--- src/public/invalid-host.tsx
+++ src/public/invalid-host.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Result } from 'antd';
 
-function InvalidHost() {
-    return (
-        <p>Invalid host!</p>
-    )
-}
+const InvalidHost: React.FC = () => (
+    <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        height: '100vh',
+        alignItems: 'center'
+    }}>
+        <Result
+            status="404"
+            title="Invalid Host"
+            subTitle="Sorry, the request host is invalid. Please contact support"
+        />
+    </div>
+);
 
-export default InvalidHost
+export default InvalidHost;
