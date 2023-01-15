4bdda085a60a96cb0eabe9081c2c66cd78706bbb
Aminul Islam <me@aminul.net>
2023-01-15 18:17:45 +0600

    cleanup

--- src/components/app-logo.tsx
+++ src/components/app-logo.tsx
@@ -3,9 +3,9 @@ import { theme } from 'antd'
 
 function AppLogo({ brand }: any) {
     const { token: { borderRadius } } = theme.useToken()
-    return (
-        <div className="app-branding" style={{ borderRadius }}>{brand}</div>
-    )
+
+    return <div className="app-branding" style={{ borderRadius }}>{brand}</div>
+
 }
 
 export default AppLogo
